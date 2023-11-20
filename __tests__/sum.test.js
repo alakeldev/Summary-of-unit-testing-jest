63f1e59a809d0754b9aca7b47979ad1b654b487e
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2023-11-20 11:52:15 +0100

    add describe to the test file and explain how to write the test inside it

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -4,27 +4,66 @@ const theSum = require("../sum");
 // it(name , function, timeout)
 // it is the short name for the test
 
-test("Return 0 if no Number", () => {
-    expect(theSum()).toBe(0);
-});
+// //test block
+// test("Return 0 if no Number", () => {
+//     expect(theSum()).toBe(0);
+// });
+// //test block
+// test("Return the Number", () => {
+//     expect(theSum(10)).toBe(10);
+// });
+// //test block
+// test("Return the Sum of num1 + num2", () => {
+//     expect(theSum(15, 25)).toBe(40);
+// });
+// //test block
+// test("Return the Sum of num1 + num2 + num3", () => {
+//     expect(theSum(10, 20, 30)).toBe(60);
+// });
+// //test block
+// test("Return the Sum of num1 + num2 + num3 + num4", () => {
+//     expect(theSum(10, 20, 30, 10)).toBe(70);
+// });
+// //test block
+// test("Return the Sum of all Numbers", () => {
+//     expect(theSum(1, 9, 10, 40, 20, 10, 100, 10)).toBe(200);
+// });
 
-test("Return the Number", () => {
-    expect(theSum(10)).toBe(10);
-});
 
-test("Return the Sum of num1 + num2", () => {
-    expect(theSum(15, 25)).toBe(40);
-});
 
-test("Return the Sum of num1 + num2 + num3", () => {
-    expect(theSum(10, 20, 30)).toBe(60);
-});
+/////////////////////////////////////////////////////////////////////
 
-test("Return the Sum of num1 + num2 + num3 + num4", () => {
-    expect(theSum(10, 20, 30, 10)).toBe(70);
-});
 
-test("Return the Sum of all Numbers", () => {
-    expect(theSum(1, 9, 10, 40, 20, 10, 100 ,10)).toBe(200);
-});
+// Describe(name, function)
+describe("check the Numbers sum Total", () => {
 
+    describe("Check if no or only one number entered", () => {
+        //test block
+        test("Return 0 if no Number", () => {
+            expect(theSum()).toBe(0);
+        });
+        //test block
+        test("Return the Number", () => {
+            expect(theSum(10)).toBe(10);
+        });
+    });
+
+    describe("check for more than one number", () => {
+        //test block
+        test("Return the Sum of num1 + num2", () => {
+            expect(theSum(15, 25)).toBe(40);
+        });
+        //test block
+        test("Return the Sum of num1 + num2 + num3", () => {
+            expect(theSum(10, 20, 30)).toBe(60);
+        });
+        //test block
+        test("Return the Sum of num1 + num2 + num3 + num4", () => {
+            expect(theSum(10, 20, 30, 10)).toBe(70);
+        });
+        //test block
+        test("Return the Sum of all Numbers", () => {
+            expect(theSum(1, 9, 10, 40, 20, 10, 100, 10)).toBe(200);
+        });
+    });
+});
